b72e16ccb063af5e68c4862f3421b9d22ad87f26
chheangg <balldremz@gmail.com>
2024-10-08 19:47:34 -0700

    add POS header

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -6,7 +6,7 @@ interface MainContentProps {
 export function MainContent({ children, isContainer = false }: MainContentProps) {
   return (
     <div className="bg-gradient-to-b from-background to-[rgba(30,41,59,0.05)] w-full h-full">
-      <div className={isContainer ? 'container' : ''}>
+      <div className={isContainer ? 'container' : 'h-full'}>
         {children}
       </div>
     </div>
--- src/routes/_page_layout/pos/index.tsx
+++ src/routes/_page_layout/pos/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button';
 import { createFileRoute } from '@tanstack/react-router';
+import { ChevronLeft, Plus } from 'lucide-react';
 
 export const Route = createFileRoute('/_page_layout/pos/')({
   component: () => <PosPage />
@@ -6,12 +8,28 @@ export const Route = createFileRoute('/_page_layout/pos/')({
 
 function PosPage() {
   return (
-    <div className='grid grid-cols-[2fr,3fr]'>
-      <div>
-        Area A
-      </div>
-      <div>
+    <div className='px-8 py-4 h-full'>
+      {/* Top Navigate Back Button */}
+      <Button variant='ghost' className='hover:bg-transparent p-0 font-semibold text-accent hover:text-primary-foreground uppercase'>
+        <span><ChevronLeft size={13} /></span>
+        Dashboard
+      </Button>
+      <div className='gap-12 grid grid-cols-[2fr,3fr] h-full'>
+        <div>
+          {/* POS Button Headers */}
+          <div className='flex justify-end gap-6'>
+            <Button className='bg-secondary hover:bg-emerald-500 px-4 rounded-[5px] font-semibold text-background'>
+              <span><Plus className='mr-1 font-bold' size={16} /></span>
+              Create Product
+            </Button>
+            <Button className='border-primary hover:bg-primary px-6 rounded-[5px] font-semibold text-primary hover:text-background' variant='outline'>
+              Clear Cart
+            </Button>
+          </div>
+        </div>
+        <div>
         Area B
+        </div>
       </div>
     </div>
   );
