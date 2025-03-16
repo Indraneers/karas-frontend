0b29a03a74b01f360cca56e1ab463e6b7edeb2bc
chheangg <balldremz@gmail.com>
2025-03-15 19:54:18 -0700

    revamp item-adder and numpads into one component

--- src/features/pos/components/item-adder.tsx
+++ src/features/pos/components/item-adder.tsx
@@ -1,29 +1,50 @@
-import { useEffect, useRef, useState } from "react";
+import { Dispatch, MouseEventHandler, SetStateAction, useEffect, useRef, useState } from "react";
 import { usePosStore } from "../store/pos";
-import { Numpad } from "./numpad";
 import { calculateTotalCost } from "@/features/sale/utils/sale";
 import { v4 as uuidv4 } from 'uuid';
 import { Currency } from "@/components/currency";
 import { convertCurrencyToInputString, convertStringToCurrency } from "@/lib/currency";
 import { Item } from "@/features/sale/types/item";
-import { ItemAdderPanelCountable } from "./item-adder-panel-countable";
-import { ItemAdderPanelVariable } from "./item-adder-panel-variable";
+import { UnderlineCurrencyInput } from "./underline-currency-input";
+import { UnderlineInput } from "./underline-input";
+import { Unit } from "@/features/unit/types/unit";
+import { convertBaseQuantityToQuantity, convertQuantityToBaseQuantity } from "@/features/unit/util/convert";
+import { ProductRequestDto } from "@/features/product/types/product.dto";
 import { ToBaseUnitSwitch } from "@/features/unit/components/to-base-unit-switch";
 import { cn } from "@/lib/utils";
+import { FormEvent } from "react";
+import { ArrowRight, Check, Delete } from "lucide-react";
 
 interface ItemAdderProps {
   item: Item;
   setOpen: (b: boolean) => void
 }
 
+interface ItemAdderPanelStateInterface {
+  price: string;
+  discount: string;
+  qty: string;
+  setPrice: Dispatch<SetStateAction<string>>;
+  setDiscount: Dispatch<SetStateAction<string>>;
+  setQty: Dispatch<SetStateAction<string>>;
+}
+
+interface ItemAdderPanelRefsInterface {
+  priceInput: React.RefObject<HTMLInputElement>;
+  discountInput: React.RefObject<HTMLInputElement>;
+  qtyInput: React.RefObject<HTMLInputElement>
+}
+
+
 export function ItemAdder({ item, setOpen }: ItemAdderProps) {
   const product = item.unit.product;
+  const isVariable = product.variable;
 
   const [isBaseUnit, setIsBaseUnit] = useState(true);
 
-  const firstInput = useRef<HTMLInputElement>(null);
-  const secondInput = useRef<HTMLInputElement>(null);
-  const thirdInput = useRef<HTMLInputElement>(null);
+  const priceInput = useRef<HTMLInputElement>(null);
+  const discountInput = useRef<HTMLInputElement>(null);
+  const qtyInput = useRef<HTMLInputElement>(null);
 
   const [price, setPrice] = useState<string>(convertCurrencyToInputString(item.price));
   const [discount, setDiscount] = useState<string>('');
@@ -31,9 +52,20 @@ export function ItemAdder({ item, setOpen }: ItemAdderProps) {
 
   const { addItem } = usePosStore();
 
-  const getterList = [price, discount, qty];
-  const setterList = [setPrice, setDiscount, setQty];
-  const refList = [firstInput, secondInput, thirdInput];
+  const stateProps: ItemAdderPanelStateInterface = {
+    price,
+    discount,
+    qty,
+    setPrice,
+    setDiscount,
+    setQty
+  };
+
+  const refProps : ItemAdderPanelRefsInterface = {
+    priceInput,
+    discountInput,
+    qtyInput
+  };
 
   const [currentElementIndex, setCurrentElementIndex] = useState(0);
 
@@ -69,28 +101,35 @@ export function ItemAdder({ item, setOpen }: ItemAdderProps) {
       handleSubmit();
     }
   }
+  
 
+  /**
+   * auto set price input as the default panel
+   */
   useEffect(() => {
-    if (firstInput && firstInput.current) {
-      firstInput.current.focus();
+    if (priceInput && priceInput.current) {
+      priceInput.current.focus();
     }
-  }, [firstInput]);
+  }, [priceInput]);
 
+  /**
+   * Handles keyboard input and switches between panels
+   */
   useEffect(() => {
     switch(currentElementIndex) {
     case 0:
-      if (firstInput && firstInput.current) {
-        firstInput.current.focus();
+      if (priceInput && priceInput.current) {
+        priceInput.current.focus();
       }
       break;
     case 1:
-      if (secondInput && secondInput.current) {
-        secondInput.current.focus();
+      if (discountInput && discountInput.current) {
+        discountInput.current.focus();
       }
       break;
     case 2:
-      if (thirdInput && thirdInput.current) {
-        thirdInput.current.focus();
+      if (qtyInput && qtyInput.current) {
+        qtyInput.current.focus();
       }
       break;
     }
@@ -120,36 +159,33 @@ export function ItemAdder({ item, setOpen }: ItemAdderProps) {
           {/* Input screen */}
           <div className="p-4">
             {
-              !product.variable &&
+              !isVariable &&
             <ItemAdderPanelCountable 
-              getterList={getterList}
-              setterList={setterList}
-              refList={refList}
+              states={stateProps}
+              refs={refProps}
               setCurrentElementIndex={setCurrentElementIndex}
             />
             }
             {
-              product.variable &&
+              isVariable &&
             <ItemAdderPanelVariable
-              getterList={getterList}
-              setterList={setterList}
-              isBaseUnit={isBaseUnit}
-              refList={refList}
+              states={stateProps}
+              refs={refProps}
               setCurrentElementIndex={setCurrentElementIndex}
               product={product}
               unit={item.unit}
+              isBaseUnit={isBaseUnit}
             />
             }
-            <div className="text-right mt-6 font-semibold text-2xl">
+            <div className="mt-6 font-semibold text-2xl text-right">
               <Currency amount={totalCost} />
             </div>
           </div>
           <Numpad 
             currentElementIndex={currentElementIndex}
             setCurrentElementIndex={setCurrentElementIndex}
-            input={refList[currentElementIndex]}
-            getter={getterList[currentElementIndex]}
-            setter={setterList[currentElementIndex]}
+            states={stateProps}
+            refs={refProps}    
             isVariable={product.variable}
             isBaseUnit={isBaseUnit}
             unit={item.unit}
@@ -157,6 +193,405 @@ export function ItemAdder({ item, setOpen }: ItemAdderProps) {
           />
         </div>
       </div>
+    </div>
+  );
+}
+
+interface ItemAdderPanelVariableProps {
+  states: ItemAdderPanelStateInterface;
+  refs: ItemAdderPanelRefsInterface
+  setCurrentElementIndex:  React.Dispatch<React.SetStateAction<number>>;
+  isBaseUnit: boolean;
+  product: ProductRequestDto;
+  unit: Unit;
+}
+
+export function ItemAdderPanelVariable({
+  states,
+  refs,
+  isBaseUnit,
+  setCurrentElementIndex,
+  product,
+  unit
+}: ItemAdderPanelVariableProps) {
+
+  /**
+   * only for price and discount
+   */
+  function onValueChange(value: string | undefined, setter: (id: string) => void) {
+    setter(value || '');
+  }
+
+  function handleQtyInput(event: FormEvent<HTMLInputElement>) {
+    const qty = convertQuantityToBaseQuantity(unit.toBaseUnit, Number(event.currentTarget.value));
+    states.setQty(qty.toString());
+  }
+  
+  function handleBaseUnitQtyInput(event: FormEvent<HTMLInputElement>) {
+    states.setQty(Number(event.currentTarget.value).toString());
+  }
+
+
+  return (
+    <div>
+      <div className="flex gap-2 text-xl">
+        <div className="flex items-baseline gap-2">
+          <span>$</span>
+          <UnderlineCurrencyInput 
+            className="w-full"
+            value={states.price}
+            onValueChange={(value) => onValueChange(value, states.setPrice)}
+            onFocus={() => setCurrentElementIndex(0)} 
+            ref={refs.priceInput} 
+          />
+          <div className="whitespace-nowrap">
+            / 1{product.baseUnit}
+          </div>
+        </div>
+        <span>
+          - 
+        </span>
+        <div className="flex items-baseline gap-2">
+          <span>$</span>
+          <UnderlineCurrencyInput
+            className="w-full"
+            value={states.discount}
+            onValueChange={(value) => onValueChange(value, states.setDiscount)}
+            onFocus={() => setCurrentElementIndex(1)} 
+            ref={refs.discountInput} 
+            type="text" 
+          />
+          <div className="whitespace-nowrap">
+          / 1{product.baseUnit}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center items-baseline gap-2 mt-2 text-xl">
+        <span>Qty</span>
+        {
+          !isBaseUnit
+          &&
+          <UnderlineInput
+            className="w-20"
+            value={convertBaseQuantityToQuantity(unit.toBaseUnit, Number(states.qty)) || ''}  
+            onInput={handleQtyInput}
+            onFocus={() => setCurrentElementIndex(2)} 
+            ref={!isBaseUnit && refs.qtyInput} 
+            type="number" 
+          />
+        }
+        {
+          isBaseUnit
+          &&
+          <UnderlineInput
+            className="w-20"
+            value={states.qty}  
+            onInput={handleBaseUnitQtyInput}
+            onFocus={() => setCurrentElementIndex(2)} 
+            ref={isBaseUnit && refs.qtyInput} 
+            type="number"
+          />
+        }
+        {isBaseUnit && product.baseUnit}
+      </div>
+    </div>
+  );
+}
+
+interface ItemAdderPanelCountableProps {
+  states: ItemAdderPanelStateInterface;
+  refs: ItemAdderPanelRefsInterface;
+  setCurrentElementIndex:  React.Dispatch<React.SetStateAction<number>>;
+}
+
+export function ItemAdderPanelCountable({
+  states,
+  refs,
+  setCurrentElementIndex
+}: ItemAdderPanelCountableProps) {
+  /**
+   * only for price and discount
+   */
+  function onValueChange(value: string | undefined, setter: (id: string) => void) {
+    setter(value || '');
+  }
+  
+  function handleQtyInput(event: FormEvent<HTMLInputElement>) {
+    states.setQty(Number(event.currentTarget.value).toString());
+  }
+  return (
+    <div className="flex gap-2 text-xl">
+      <div className="flex items-baseline gap-2">
+        <span>$</span>
+        <UnderlineCurrencyInput 
+          className="w-full"
+          value={states.price}
+          onValueChange={(value) => onValueChange(value, states.setPrice)}
+          onFocus={() => setCurrentElementIndex(0)} 
+          ref={refs.priceInput} 
+        />
+      </div>
+      <span>
+    - 
+      </span>
+      <div className="flex items-baseline gap-2">
+        <span>$</span>
+        <UnderlineCurrencyInput
+          className="w-full"
+          value={states.discount}
+          onValueChange={(value) => onValueChange(value, states.setDiscount)}
+          onFocus={() => setCurrentElementIndex(1)} 
+          ref={refs.discountInput} 
+          type="text" 
+        />
+      </div>
+      <div className="flex items-baseline gap-2">
+        <span>Qty</span>
+        <UnderlineInput
+          className="w-full"
+          value={states.qty}  
+          onInput={handleQtyInput}
+          onFocus={() => setCurrentElementIndex(2)} 
+          ref={refs.qtyInput} 
+        />
+      </div>
+    </div>
+  );
+}
+
+interface NumpadProps {
+  states: ItemAdderPanelStateInterface;
+  refs: ItemAdderPanelRefsInterface;
+  handleSubmit: () => void;
+  currentElementIndex: number;
+  setCurrentElementIndex: (i: number) => void;
+  isBaseUnit: boolean;
+  isVariable: boolean;
+  unit: Unit;
+}
+
+export function Numpad({ 
+  handleSubmit, 
+  currentElementIndex, 
+  setCurrentElementIndex,
+  isBaseUnit,
+  isVariable,
+  unit,
+  states,
+  refs
+}: NumpadProps) {
+
+  function isMoreThanTwoDigit(t: string) {
+    const textArray = t.split('.');
+    if (textArray[1] != null) {
+      return textArray[1].length > 2;
+    }
+    return false;
+  }
+  
+  function isValidCurrencyInput(t: string) {
+    const validPattern = /^(\d+(\.\d{1,2})?|\d+\.|)$/;
+    return validPattern.test(t);
+  }
+  
+  function isValidCurrencyValue(t: string) {
+    return isValidCurrencyInput(t) && !isMoreThanTwoDigit(t);
+  }
+
+  function getSetter(currentElementIndex: number) {
+    switch (currentElementIndex) {
+    case 0:
+      return states.setPrice;
+    case 1:
+      return states.setDiscount;
+    case 2:
+      return states.setQty;
+    }
+  }
+
+  function getGetter(currentElementIndex: number) {
+    switch (currentElementIndex) {
+    case 0:
+      return states.price;
+    case 1:
+      return states.discount;
+    case 2:
+      return states.qty;
+    }
+  }
+
+  function getInput(currentElementIndex: number) {
+    switch (currentElementIndex) {
+    case 0:
+      return refs.priceInput;
+    case 1:
+      return refs.discountInput;
+    case 2:
+      return refs.qtyInput;
+    }
+  }
+
+  function handleNumpadKey(key: string | number) {  
+    const getter = getGetter(currentElementIndex)!;
+    const setter = getSetter(currentElementIndex)!;
+    const input = getInput(currentElementIndex)!;
+
+    if (!input || !input.current) {
+      return;
+    }
+
+    let newInput = input.current.value;
+
+    if (currentElementIndex == 0 || currentElementIndex == 1) {
+
+      if (key === 'd') {
+        newInput = newInput.slice(0, -1);
+      }
+      else {
+        newInput += key;
+      }
+    }
+    else if (currentElementIndex == 2) {
+      const isDirectChange = isBaseUnit || !isVariable;
+
+      if (key === 'd') {
+        if (isDirectChange)  {
+          newInput = newInput.slice(0, -1);
+        }
+        else {
+          const newQty = String(newInput).slice(0, -1);
+          newInput = String(convertQuantityToBaseQuantity(unit.toBaseUnit, Number(newQty)));
+        }
+      }
+      else if (key === '.') {
+        if (isDirectChange) {
+          newInput += key;
+        }
+      }
+      else {
+        if (isDirectChange) {
+          newInput += key;
+        }
+        else {
+          const newQty = newInput + key;
+          newInput = String(convertQuantityToBaseQuantity(unit.toBaseUnit, Number(newQty)));
+        }
+      }
+      setter(newInput);
+      return;
+    }
+
+    if (!isValidCurrencyValue(newInput)) {
+      setter(getter);
+    }
+    else {
+      setter(newInput);
+    }
+  }
+  return (
+    <div className="grid grid-cols-3 grid-rows-3 bg-background rounded-[2rem] text-3xl">
+      <NumpadKey
+        onClick={() => handleNumpadKey(7)}
+        className="rounded-tl-[2rem]"
+      >
+      7
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(8)}
+      >
+      8
+      </NumpadKey>
+      <NumpadKey 
+        onClick={() => handleNumpadKey(9)}
+        className="rounded-tr-[2rem]"
+      >
+      9
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(4)}
+      >
+      4
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(5)}
+      >
+      5
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(6)}
+      >
+      6
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(1)}
+      >
+      1
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(2)}
+      >
+      2
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(3)}
+      >
+      3
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey('d')}
+      >
+        <Delete />
+      </NumpadKey>
+      <NumpadKey
+        onClick={() => handleNumpadKey(0)}
+      >
+      0
+      </NumpadKey>
+      <NumpadKey
+        className={cn([
+          currentElementIndex === 2 && isVariable && !isBaseUnit && 'bg-foreground/20 hover:bg-foreground/20 cursor-auto hover:text-foreground'
+        ])}
+        onClick={() => handleNumpadKey('.')}
+      >
+      .
+      </NumpadKey>
+      {
+        !(currentElementIndex === 2) &&
+      <NumpadKey 
+        onClick={() => setCurrentElementIndex(currentElementIndex + 1)}
+        className="col-span-3 bg-amber-500 hover:bg-amber-400 py-4 rounded-b-[2rem] aspect-auto text-background"
+      >
+        <ArrowRight />
+      </NumpadKey>
+      }
+      {
+        currentElementIndex === 2 &&
+      <NumpadKey 
+        className="col-span-3 bg-green-500 hover:bg-green-400 py-4 rounded-b-[2rem] aspect-auto text-background"
+        onClick={() => handleSubmit()}
+      >
+        <Check />
+      </NumpadKey>
+      }
+    </div>
+  );
+}
+
+interface NumpadKeyProps {
+  className?: string;
+  children: React.ReactNode;
+  onClick?: MouseEventHandler<HTMLDivElement>
+}
+
+export function NumpadKey({ children, className, onClick }: NumpadKeyProps) {
+  return (
+    <div 
+      onClick={onClick}
+      className={cn([
+        "place-content-center grid text-foreground aspect-square hover:bg-accent/80 hover:text-background cursor-pointer",
+        className
+      ])}>
+      {children}
     </div>
   );
 }
