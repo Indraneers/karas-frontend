08bd26d7342106090c208b43f4ef5417ae4252d6
chheangg <balldremz@gmail.com>
2026-03-02 20:49:14 -0800

    feat: good logging practice and ui improvement

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "flex bg-white file:bg-transparent disabled:opacity-50 px-3 py-1 border border-border file:border-0 rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring w-full h-9 file:font-medium placeholder:text-muted-foreground file:text-foreground md:text-sm file:text-sm text-base transition-colors disabled:cursor-not-allowed",
+        "flex bg-white file:bg-transparent disabled:opacity-50 px-3 py-1.5 border border-border file:border-0 rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring w-full h-9 file:font-medium placeholder:text-muted-foreground file:text-foreground md:text-sm file:text-sm text-base transition-colors disabled:cursor-not-allowed",
         className,
       )}
       {...props}
