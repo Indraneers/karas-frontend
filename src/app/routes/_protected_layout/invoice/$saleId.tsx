427731ad4fd67b6d4eba5e902adeff6d26c4e2de
chheangg <balldremz@gmail.com>
2026-04-02 17:02:09 -0700

    fix: test out a4 fix natively

--- src/app/routes/_protected_layout/invoice/$saleId.tsx
+++ src/app/routes/_protected_layout/invoice/$saleId.tsx
@@ -12,8 +12,7 @@ import { convertSaleResponseDtoToSale } from "@/features/sale/utils/sale";
 import { cn } from "@/lib/utils";
 import { useQueries } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useRef, useState } from "react";
-import { useReactToPrint } from "react-to-print";
+import { useEffect, useRef } from "react";
 import { format } from "date-fns";
 import { getConfig } from "@/features/app-config/api/app-config";
 
@@ -48,32 +47,18 @@ export function InvoicePage() {
   });
 
   const contentRef = useRef<HTMLDivElement>(null);
-  const reactToPrintFn = useReactToPrint({
-    contentRef,
-    onAfterPrint: () => {
-      window.close();
-    },
-  });
-
-  const [isReadyToPrint, setIsReadyToPrint] = useState(false);
 
   useEffect(() => {
     if (print && saleQuery.isSuccess && configQuery.isSuccess) {
       const raf = requestAnimationFrame(() => {
-        const raf2 = requestAnimationFrame(() => {
-          setIsReadyToPrint(true);
+        requestAnimationFrame(() => {
+          window.print();
+          window.close();
         });
-        return () => cancelAnimationFrame(raf2);
       });
       return () => cancelAnimationFrame(raf);
     }
   }, [configQuery.isSuccess, print, saleQuery.isSuccess]);
-
-  useEffect(() => {
-    if (isReadyToPrint) {
-      reactToPrintFn();
-    }
-  }, [isReadyToPrint, reactToPrintFn]);
 
   if (saleQuery.isError || configQuery.isError) {
     return "error";
